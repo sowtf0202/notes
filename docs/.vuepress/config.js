96ae58d993568fe346d851b2a8d48b3d916463c0
sowtf <yao.li@ifchange.com>
2020-05-14 00:57:45 +0800

    docs: 更新 description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   base: '/notes/',
-  title: 'sowtf',
-  description: 'vuepress notes',
+  title: 'sowtf\'s notes',
+  description: 'sowtf\'s vuepress notes, sowtf, so what the fuck, 学习前端, vue 学习',
   themeConfig: {
     repo: 'https://github.com/sowtf0202/notes', //git 仓库地址
     repoLabel: 'github', // 自定义仓库链接文字
-    lastUpdated: '上次更新',
+    lastUpdated: 'last updated',
     smoothScroll: true,
     displayAllHeaders: true,
     // navbar: false,
